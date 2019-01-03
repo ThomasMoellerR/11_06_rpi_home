62354a73969086be4a8bef8a18bbc24fb3d437cc
Your Name <you@example.com>
2019-01-03 12:36:40 +0100

    test

---
+++ main.sh
@@ -0,0 +1,3 @@
+sudo python3 "/home/pi/SmartHome/main.py" \
+--mqtt_server_ip "192.168.2.52" \
+--mqtt_server_port "1883" \
---
+++ tmux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+tmux new-session -d -s my_session "bash /home/pi/SmartHome/main.sh"
+exit 0
