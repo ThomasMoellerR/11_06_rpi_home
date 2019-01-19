ed9a920a84c62f24fd97bb4c23ce3a7710f5b2a7
Your Name <you@example.com>
2019-01-19 17:05:01 +0100

    test

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
-sudo python3 "/home/pi/a/SmartHome/main.py" \
+sudo python3 "/home/pi/a/home/main.py" \
 --mqtt_server_ip "192.168.2.52" \
 --mqtt_server_port "1883" \
--- tmux.sh
+++ tmux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tmux new-session -d -s smarthome "bash /home/pi/a/SmartHome/main.sh"
+tmux new-session -d -s smarthome "bash /home/pi/a/home/main.sh"
 exit 0
